43f499ca3f1fe7ae19a2e3f106955a6a82f3abe8
Zaki <BSI80135@bsi.co.id>
2022-10-11 09:42:31 +0700

    added sessionstorage and dialog component

--- Services/OvertimeService.cs
+++ Services/OvertimeService.cs
@@ -24,6 +24,7 @@ namespace PersonaOvertimeWeb.Services
             String token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJuYW1lIjoiSWthd2F0aSBCYXNyaSIsIm5iZiI6MTY2NTM2NDk2OCwiZXhwIjoxNjY1NjI0MTY4LCJpYXQiOjE2NjUzNjQ5Njh9.5WmqAjGIvMPfskp52po_Q32e-NWM1Yhgq_EXWE_guZBVcbOsGzDm6ejBvc_G4PFk2nigpt511g6fITLPWNrvVg";
             _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
             return await _httpClient.GetFromJsonAsync<SuccessResponse<OvertimeStatistic>>("/overtime/statistic");
+        }
 
         public async Task<HttpResponseMessage> PostOvertimeRequest(MultipartFormDataContent request)
         {
