6632d71ae585874859d1e518c4e9e21235d92f30
ikawatibasri <ikawati.basri@gmail.com>
2022-10-11 08:11:37 +0700

    update homepage

--- Services/IOvertimeService.cs
+++ Services/IOvertimeService.cs
@@ -7,6 +7,7 @@ namespace PersonaOvertimeWeb.Services
         Task<List<OvertimeDTO>> GetAllOvertimeList();
 
         Task<SuccessResponse<OvertimeStatistic>> GetOvertimeStatistic();
+        Task<SuccessResponse<OvertimeStatistic>> GetOvertimeStatisticSuperior();
 
         Task<HttpResponseMessage> PostOvertimeRequest(MultipartFormDataContent request);
 
